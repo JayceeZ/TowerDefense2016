a5ef1852234a56087217eec7319c89f60d527895
Jean-Christophe Isoard <jeanchristophe.isoard@gmail.com>
2016-01-08 11:30:46 +0100

    TUIO messages to server + new clients

--- Server/server.js
+++ Server/server.js
@@ -4,15 +4,15 @@ var socketio = require('socket.io');
 
 app.use(express.static(__dirname + '/'));
 
-app.use( function(req, res, next) {
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
 });
 
-app.get('/', function (req, res, next) {
-    res.render('index.html');
+app.get('/', function(req, res, next) {
+  res.render('index.html');
 });
 
 var port = 8080;
@@ -26,10 +26,10 @@ init();
 console.log("Server started");
 
 function init() {
-    console.log("Initialisation");
+  console.log("Initialisation");
 
-    players = [];
-    tableSocket = null;
+  players = [];
+  tableSocket = null;
 }
 
 // launch the http server on given port
@@ -41,30 +41,43 @@ app.listen(port);
 var ioServer = socketio(8081);
 
 
-ioServer.on('connection', function (socket) {
-    socket.on('log', function(content) {
-      console.log(content + " received from " + socket.id);
-      idevSocket.join('idev');
-      idevSocket.emit('idevLog', content);
-    });
+ioServer.on('connection', function(socket) {
+  socket.on('log', function(content) {
+    console.log(content + " received from " + socket.id);
+    idevSocket.join('idev');
+    idevSocket.emit('idevLog', content);
+  });
 
-    socket.on('addTable', function () {
-        tableSocket = socket;
-    });
+  socket.on('table', function(message) {
+    socket.to('players').emit(message);
+    socket.to('caca').emit(message);
+  });
 
-    socket.on('addIdev', function () {
-        idevSocket = socket;
-    });
+  socket.on('addTable', function() {
+    tableSocket = socket;
+  });
 
-    socket.on('addPlayer', function (playerName) {
-        var player = {socket: socket, id: players.length, name: playerName};
+  socket.on('addHandDevice', function() {
+    socket.join('players');
+  });
 
-        players.push(player);
+  socket.on('addCaca', function() {
+    socket.join('caca');
+  });
 
-        socket.emit('idevLog', "Player " + player.id + " on " + players.length);
-    });
+  socket.on('addIdev', function() {
+    idevSocket = socket;
+  });
 
-    socket.on('disconnect', function () {
-        console.log("PERTE DE CONNEXION");
-    });
+  socket.on('addPlayer', function(playerName) {
+    var player = {socket: socket, id: players.length, name: playerName};
+
+    players.push(player);
+
+    socket.emit('idevLog', "Player " + player.id + " on " + players.length);
+  });
+
+  socket.on('disconnect', function() {
+    console.log("PERTE DE CONNEXION");
+  });
 });
--- Table/server.js
+++ Table/server.js
@@ -1,11 +1,25 @@
-/****************
- * OSC Over UDP *
- ****************/
+var osc = require('node-osc'),
+    io = require('socket.io-client');
 
-var osc = require('node-osc');
+/***************
+ * TUIO Events *
+ ***************/
 
 var oscServer = new osc.Server(3333, '192.168.1.8');
 oscServer.on("message", function (msg) {
   console.log("TUIO message:");
   console.log(msg);
+});
+
+
+/************************
+ * Server communication *
+ ************************/
+
+var socket = io.connect("http://192.168.1.18:8081");
+
+socket.emit('addTable');
+
+socket.on('idevMessage', function (device) {
+  document.getElementById("devices").innerHTML += JSON.stringify(device);
 });
