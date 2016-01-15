6cbb6356fb2f8e13ab98be7813a9dab2e0079429
Jean-Christophe Isoard <jeanchristophe.isoard@gmail.com>
2016-01-15 08:48:31 +0100

    Fix code/files duplication

--- Table/app/modules/map/controllers.js
+++
@@ -1,9 +0,0 @@
-/*
- * @author Jean-Christophe Isoard
- */
-
-var mapModule = angular.module("mapModule", []);
-
-mapModule.controller("mapCtrl", function($scope) {
-    $scope.helloworld = "Hello World !"
-});
