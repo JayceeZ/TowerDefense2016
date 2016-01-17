7e9bb49cdb5315549b4ec5a519cd92e7c84661f7
JayceeZ <jeanchristophe.isoard@gmail.com>
2016-01-17 10:44:39 +0100

    Slots rightly positionned and first titles

--- Table/src/model/Slot.js
+++ Table/src/model/Slot.js
@@ -6,6 +6,7 @@ var Slot = function Slot(id) {
   this.id = id;
   this.x = 0;
   this.y = 0;
+  this.color = [0, 0, 0, 0.2];
 
   this.setX = function(x) {
     this.x = x;
@@ -14,4 +15,8 @@ var Slot = function Slot(id) {
   this.setY = function(y) {
     this.y = y;
   };
+
+  this.getColorCSS = function() {
+    return "rgba("+this.color[0]+","+this.color[1]+","+this.color[2]+","+this.color[3]+")";
+  }
 };
