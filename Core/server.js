86b0b07212b6b850ffa98ed637e036138dac0118
Alex Tis <alex.tis59@gmail.com>
2016-01-29 13:32:28 +0100

    Correction validate

--- Core/server.js
+++ Core/server.js
@@ -96,7 +96,7 @@ socket.on('putTower', function(idplayer){
   if(marker != null && marker.positionOk == true){
     var tower = game.addTower(idplayer,marker.x,marker.y,marker.angle);
     if(tower !== null)
-      socket.emit("validateTower",{"idplayer":idplayer,"id":tower.id,"x":tower.x,"y":tower.y,"angle":tower.angle,"type":tower.type});
+      socket.emit("validateTower",{"playerId":idplayer,"id":tower.id,"x":tower.x,"y":tower.y,"angle":tower.angle,"type":tower.type});
   }
 });
 
