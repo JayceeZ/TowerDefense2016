24b0cb9742489ba458629c9e606c90196dbdc233
Jean-Christophe Isoard <jeanchristophe.isoard@gmail.com>
2016-01-29 13:13:54 +0100

    this.scope not $scope

--- TableUI/src/model/Map.js
+++ TableUI/src/model/Map.js
@@ -47,7 +47,7 @@ var Map = function Map(scope, container) {
   this.setPlayerTurretSpecs = function(idplayer, aimZone) {
     var turret = new Turret(idplayer, this.container);
     turret.setAimZone(aimZone.distance, aimZone.arc);
-    var marker = _.find($scope.markers, {player: idplayer});
+    var marker = _.find(this.scope.markers, {player: idplayer});
     if(marker) {
       turret.setPosition(marker.x, marker.y);
       turret.setOrientation(marker.orientation);
