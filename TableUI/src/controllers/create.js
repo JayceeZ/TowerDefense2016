2326eaa9dc2f1539728a23462871d6db13335df8
Jean-Christophe Isoard <jeanchristophe.isoard@gmail.com>
2016-02-12 08:53:58 +0100

    Might be a fix again

--- TableUI/src/controllers/create.js
+++ TableUI/src/controllers/create.js
@@ -38,8 +38,7 @@ appTable.controller('CreateCtrl', function($scope, $location, socket) {
   socket.on('addPlayer', function(message) {
     console.log("addPlayer : "+message.id+" "+message.pseudo);
     var freeSlot = getFirstFreeSlot();
-    var assoc = freeSlot.setPlayer(message.id);
-    if (assoc) {
+    if (freeSlot.setPlayer(message.id)) {
       socket.emit('playerColorUpdate', {id: freeSlot.player, pseudo: freeSlot.playerPseudo, color: freeSlot.color});
       freeSlot.setPlayerPseudo(message.pseudo);
     }
@@ -62,9 +61,8 @@ appTable.controller('CreateCtrl', function($scope, $location, socket) {
       var y = message.y * create[0].clientHeight;
       var slot = getSlot(x, y);
       // Only if a player is connected in the slot
-      if (slot && slot !== null) {
-        var assoc = slot.setTag(message.id);
-        if (assoc)
+      if (slot) {
+        if (slot.setTag(message.id))
           socket.emit('playerColorUpdate', {id: slot.player, pseudo: slot.playerPseudo, color: slot.color});
       }
     }
